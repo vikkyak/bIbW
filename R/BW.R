db2377659751d8d9f6a19875d2007829068a53d9
vIkAsSiNgH <76863611+vikkyak@users.noreply.github.com>
2024-05-28 12:21:03 +0900

    Update BW.R

--- R/BW.R
+++ R/BW.R
@@ -1,48 +1,45 @@
-BW <- function( NonZeroGenes, pc, Method = NULL){ 
-  
-  
+BW <- function(NonZeroGenes, pc, Method = NULL) {
   if (Method == "KernSmooth") {
-  hh <- sapply( NonZeroGenes, function(x) {
-    KernSmooth::dpik(pc[x[1,]])
-  })
-}
+    hh <- sapply(NonZeroGenes, function(x) {
+      KernSmooth::dpik(pc[x[1, ]])
+    })
+  }
   else if (Method == "SJ")  {
-    hh <- sapply( NonZeroGenes, function(x) {
+    hh <- sapply(NonZeroGenes, function(x) {
       h <- tryCatch({
-        bw.SJ(pc[x[1, ]], method = "ste")
+        bw.SJ(pc[x[1,]], method = "ste")
       }, error = function(e) {
-        return(bw.nrd0(pc[x[1, ]]))
+        return(bw.nrd0(pc[x[1,]]))
       })
     })
   }
   
   else if (Method == "ucv") {
-    hh <- sapply( NonZeroGenes, function(x) {
+    hh <- sapply(NonZeroGenes, function(x) {
       h <- tryCatch({
-        bw.ucv(pc[x[1, ]])
+        bw.ucv(pc[x[1,]])
       }, error = function(e) {
-        return(bw.nrd0(pc[x[1, ]]))
+        return(bw.nrd0(pc[x[1,]]))
       })
     })
   }
   
   else if (Method == "bcv") {
-    hh <- sapply( NonZeroGenes, function(x) {
+    hh <- sapply(NonZeroGenes, function(x) {
       h <- tryCatch({
-        bw.bcv(pc[x[1, ]])
+        bw.bcv(pc[x[1,]])
       }, error = function(e) {
-        return(bw.nrd0(pc[x[1, ]]))
+        return(bw.nrd0(pc[x[1,]]))
       })
     })
   }
   
   else {
-    
-    hh <- sapply( NonZeroGenes, function(x) {
+    hh <- sapply(NonZeroGenes, function(x) {
       h <- tryCatch({
-        bw.nrd(pc[x[1, ]])
+        bw.nrd(pc[x[1,]])
       }, error = function(e) {
-        return(bw.nrd0(pc[x[1, ]]))
+        return(bw.nrd0(pc[x[1,]]))
       })
     })
   }
