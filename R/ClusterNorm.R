d6fda039739cb0288688186c10cf8a053ec79613
vIkAsSiNgH <76863611+vikkyak@users.noreply.github.com>
2024-05-28 12:21:32 +0900

    Update ClusterNorm.R

--- R/ClusterNorm.R
+++ R/ClusterNorm.R
@@ -1,22 +1,27 @@
-ClusterNorm <- function(X, GeneFilter = NULL, scaling = NULL, Method = NULL, cutoff = NULL ){
-  
-  if (is_sparse(X)) {
-    X <- as(X, "dgCMatrix")
+ClusterNorm <-
+  function(X,
+           GeneFilter = NULL,
+           Scaling = NULL,
+           Method = NULL,
+           Thresh = NULL) {
+    if (is_sparse(X)) {
+      X <- as(X, "dgCMatrix")
+    }
+    else {
+      X <- as.matrix(X)
+    }
+    if (is.null(Scaling)) {
+      Scaling <- colSums(X)
+    }
+    if (any(Scaling == 0)) {
+      stop("cells should have non-zero library sizes or 'Scaling' values")
+    }
+    eigenV <- svd(X[GeneFilter, , drop = FALSE])[[1]]
+    CountData <- X[GeneFilter, , drop = FALSE] / eigenV[1]
+    ## Multifactor analysis of each conditions
+    SF <- MultiFactor(CountData[, , drop = FALSE], Method, Thresh)
+    Scaling <- Scaling / mean(eigenV)
+    SF <- SF * sqrt(Scaling)
+    X <- t(t(X[, , drop = FALSE]) / SF)
+    list(X = X, SF = SF)
   }
-  else {
-    X <- as.matrix(X)
-  }
-  if (is.null(scaling)) {
-    scaling <- colSums(X)
-  }
-  if (any(scaling == 0)) {
-    stop("cells should have non-zero library sizes or 'scaling' values")
-  }
-  eigenV <- svd(X[GeneFilter, , drop = FALSE])[[1]]
-  CountData <- X[GeneFilter, , drop = FALSE] / eigenV[1]
-  SF <- MultiFactor(CountData[, , drop = FALSE], Method, cutoff)
-  scaling <- scaling / mean(eigenV)
-  SF <- SF * sqrt(scaling)
-  X <- t(t(X[, , drop = FALSE]) / SF)
-  list(X = X, SF = SF)
-}
